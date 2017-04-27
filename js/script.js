71e6672176e0d977aabdacf60bdb6c15a8398e9d
ryandnieves <ryandeannieves@gmail.com>
2017-04-27 01:01:03 -0400

    update entry php file

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ $('.editEntry').click(function() {
     var oldText = $this.parent().parent().find('p').text(); 
     var id = $this.parent().parent().find('#id').val(); 
     $this.parent().parent().find('p').empty().append('<textarea class="newDescription" cols="33">' + oldText + '</textarea>'); 
+    $('.newDescription').blur(function() { 
+        var newText = $(this).val(); 
+        $.ajax({ 
+            type: 'POST', 
+            url: 'updateEntry.php', 
+            data: 'description=' + newText + '&id=' + id, 
+              
+            success: function(results) { 
+                $this.parent().parent().find('p').empty().append(newText); 
+            } 
+        }); 
+    }); 
+    return false; 
+}); 
+});
