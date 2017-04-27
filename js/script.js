8fb68d6461c38c9e9d4e71b8eec4d61b4a4e41ce
ryandnieves <ryandeannieves@gmail.com>
2017-04-27 01:05:41 -0400

    adding a delete function

--- js/script.js
+++ js/script.js
@@ -18,4 +18,19 @@ $('.editEntry').click(function() {
     }); 
     return false; 
 }); 
+// Delete anchor tag clicked 
+$('a.deleteEntryAnchor').click(function() { 
+    var thisparam = $(this); 
+    thisparam.parent().parent().find('p').text('Please Wait...'); 
+    $.ajax({ 
+        type: 'GET', 
+        url: thisparam.attr('href'), 
+          
+        success: function(results){ 
+            thisparam.parent().parent().fadeOut('slow'); 
+        } 
+    }) 
+    return false; 
+});
+
 });
