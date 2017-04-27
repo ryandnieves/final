9a0abde15294135cbb56c47f3968ad270dceb2b0
ryandnieves <ryandeannieves@gmail.com>
2017-04-27 01:02:31 -0400

    accept id method

--- db.php
+++ db.php
@@ -7,4 +7,18 @@ class Db {
         $this->mysql = new mysqli('localhost', 'root', 'yourPassword', 'db') or die('problem'); 
     } 
 } // end class
+
+function update_by_id($id, $description) { 
+    $query = "UPDATE todo  
+              SET description = ?  
+          WHERE id = ? 
+              LIMIT 1"; 
+            
+    if($stmt = $this->mysql->prepare($query)) { 
+        $stmt->bind_param('si', $description, $id); 
+        $stmt->execute(); 
+        return 'good job! Updated'; 
+    } 
+}
+
 ?>
